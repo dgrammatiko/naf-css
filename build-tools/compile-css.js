49d2bd65abecb4e2dc4fb439468cb7d3c784518f
dgrammatiko <d.grammatiko@gmail.com>
2022-06-06 18:38:23 +0200

    fixes the wrong assets published

--- build-tools/compile-css.js
+++ build-tools/compile-css.js
@@ -7,7 +7,6 @@
  */
 const {readFile, writeFile} = require('fs').promises;
 const brotliSize = require('brotli-size');
-const gzipSize = require('gzip-size');
 const postcss = require('postcss');
 const kleur = require('kleur');
 
@@ -24,13 +23,13 @@ module.exports.compile = async (file, flags) => {
   // Non minified
   if (flags.nonMinified) {
     await writeFile(`${file.replace('src', 'css').replace('.css', '')}${flags.legacy ? '-ie' : ''}.css`, css);
-    process.stdout.write(kleur.green(`File ${file.replace('src', 'css')}${flags.legacy ? '-ie' : ''}.css [Brotli-size=${brotliSize.sync(css)}, GZip-size=${gzipSize.sync(css)}, Uncompressed-size=${css.length}] was created succesfully 👍 `) + "\n");
+    process.stdout.write(kleur.green(`File ${file.replace('src', 'css')}${flags.legacy ? '-ie' : ''}.css [Brotli-size=${brotliSize.sync(css)}, Uncompressed-size=${css.length}] was created succesfully 👍 `) + "\n");
   }
 
   // Minified
   if (flags.minified) {
     const min = await postcss(minOpts).process(css, { from: file });
     await writeFile(`${file.replace('src', 'css').replace('.css', '')}${flags.legacy ? '-ie' : ''}.min.css`, min.css);
-    process.stdout.write(kleur.green(`File ${file.replace('src', 'css').replace('.css', '')}${flags.legacy ? '-ie' : ''}.min.css [Brotli-size=${brotliSize.sync(min.css)}, GZip-size=${gzipSize.sync(min.css)}, Uncompressed-size=${min.css.length}] was created succesfully 👍 `) + "\n");
+    process.stdout.write(kleur.green(`File ${file.replace('src', 'css').replace('.css', '')}${flags.legacy ? '-ie' : ''}.min.css [Brotli-size=${brotliSize.sync(min.css)}, Uncompressed-size=${min.css.length}] was created succesfully 👍 `) + "\n");
   }
 }
